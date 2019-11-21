665de2b28ec2e4b7d99646d0cc3c05ab43587037
Liraz Gur-Arie <lirazgur-arie@Crowds-iMac.local>
2019-11-21 15:32:01 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+          <div className="test">
+               <div className="test__child">
+                    <div className="test__child__of-child">
+
+                    </div>
+               </div>
+          </div>
+    </div>
+  );
+}
+
+export default App;
